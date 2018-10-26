238c6d4371db9a8905b64af4d94fd1ef5b331f1d
Matthew Sherman <msherman0801@gmail.com>
2018-10-26 21:53:46 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -37,7 +37,7 @@ end
 def selective_tweet_shortener(tweet)
   if tweet.length > 140
     word_substituter(tweet)
-  elsif tweet.length < 14
+  elsif tweet.length < 140
     word_substituter(tweet)
   end
 end
